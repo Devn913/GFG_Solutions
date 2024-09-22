7e9172d9c1523c621002c5499742ed715f80c804
Dev Nirwal <devnirwal16@gmail.com>
2024-09-22 19:23:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.lps(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int lps(String pat) {
+        // code here
+        int M = pat.length();
+        int[] lps = new int[M];
+        // Length of the previous longest prefix suffix
+        int len = 0;
+      
+        // lps[0] is always 0
+        lps[0] = 0;
+
+        // Loop calculates lps[i] for i = 1 to M-1
+        int i = 1;
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps[M - 1];
+        
+    }
+}
