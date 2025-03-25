5517a25580474e0f4319cba6b3c1a1720fa8b78d
Dev Nirwal <devnirwal16@gmail.com>
2025-03-25 18:34:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.java
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -37,36 +37,28 @@ class Main {
 
 
 
+
+
 class Solution {
     public static ArrayList<Integer> calculateSpan(int[] arr) {
 
+        
         int n = arr.length; 
-        ArrayList<Integer> span = new ArrayList<>(
-                              Collections.nCopies(n, 0));  
-        Stack<Integer> stk = new Stack<>();  
-
-        // Process each day's price
-        for (int i = 0; i < n; i++) {
-
-            // Remove elements from the stack while the current price 
-            // is greater than or equal to stack's top price
-            while (!stk.isEmpty() && arr[stk.peek()] <=
-                                        arr[i]) {
-                stk.pop();
+        ArrayList<Integer> span = new ArrayList<>(Collections.nCopies(n, 0));  
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0;i<n;i++){
+            
+            while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+                s.pop();
             }
-
-            // If stack is empty, all elements to the left are smaller
-            // Else, top of the stack is the last greater element's index
-            if (stk.isEmpty()) {
-                span.set(i, (i + 1));
-            } else {
-                span.set(i, (i - stk.peek()));
+            
+            if(s.isEmpty()){
+                span.set(i,i+1);
+            }else{
+                span.set(i,i - s.peek());
             }
-
-            // Push the current index to the stack
-            stk.push(i);
+            s.push(i);
         }
-
         return span;
     }
 }
