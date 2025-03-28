7f08f2ebed485f3de60d23c59eb196032e48e399
Dev Nirwal <devnirwal16@gmail.com>
2025-03-28 20:55:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node reverse(Node head){
+        Node prev = null;
+        Node curr = head;
+        Node next = head.next;
+        while(next!=null){
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            next = next.next;
+        }
+        curr.next = prev;
+        return curr;
+    }
+    public Node addOne(Node head) {
+        if(head == null) return new Node(1);
+        // code here.
+        head = reverse(head);
+        Node temp = head;
+        int carry = 1;
+        while(carry != 0 && temp!=null){
+            int val = temp.data + carry;
+            carry = val/10;
+            temp.data = val%10;
+            if(temp.next == null && carry!=0){
+                temp.next = new Node(carry);
+                return reverse(head);
+            }
+            temp = temp.next;
+            
+        }
+        return reverse(head);
+    }
+}
