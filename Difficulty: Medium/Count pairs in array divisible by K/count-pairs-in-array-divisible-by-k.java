6659ef0436014e3326c294e6d3eddfde355e8e3b
Dev Nirwal <devnirwal16@gmail.com>
2025-04-28 11:31:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count pairs in array divisible by K/count-pairs-in-array-divisible-by-k.java
+++ Difficulty: Medium/Count pairs in array divisible by K/count-pairs-in-array-divisible-by-k.java
@@ -29,28 +29,25 @@ System.out.println("~");
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution
-{
-    public static long countKdivPairs(int arr[], int n, int k)
-    {
-        //code here
+class Solution {
+    public static long countKdivPairs(int arr[], int n, int k) {
+        // code here
         long ans = 0;
         int[] rem = new int[k];
-        for(int i = 0;i<n;i++){
-            rem[arr[i]%k]++;
+        for(int num: arr){
+            rem[num%k]++;
         }
         
-        for(int i = 0;i+i<k;i++){
-            if(i == 0) ans = ans + (rem[0]*(rem[0]-1)/2);
-            else ans = ans+ (rem[i]*rem[k-i]);
-        }
-        if(k%2 == 0){
-            ans = ans + (rem[k/2]*(rem[k/2]-1)/2);
+        for(int i = 1;i+i<k;i++){
+            ans+= (rem[i]*rem[k-i]);
         }
 
+        ans += (rem[0] * (rem[0]-1) /2);
+        if(k%2 == 0){
+            ans += (rem[k/2] * (rem[k/2]-1)/2);
+        }
         return ans;
-        
     }
 }
